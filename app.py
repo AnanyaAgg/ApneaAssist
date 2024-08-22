b2bd43ef60c3852aefcb8f3b009229432970c461
Ananya Aggarwal <60582678+AnanyaAgg@users.noreply.github.com>
2024-08-21 22:41:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from google.cloud import firestore
 import time
 
 # Authenticate to Firestore with the JSON account key.
-db = firestore.Client.from_service_account_json("firestore-key.json")
+import json
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="ApneaAssist")
 
 # Create a reference to the Google post.
 doc_ref = db.collection("userData").document("4T8rJNJL3JwgjsGr5uaU")
